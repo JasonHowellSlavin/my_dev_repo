25d6faba7a1db715030906047ea3d3e2d34a3dcd
Jason Slavin <slavin.jhs@gmail.com>
2017-01-09 22:16:27 -0800

    Created Player Object

--- scripts/index_js.js
+++ scripts/index_js.js
@@ -1,7 +1,17 @@
 window.onload( function () {
 	console.log("You have the bridge commander");	
 
+function Player (name) {
+	this.name = name,
+	this.chipCount = 0,
+	this.handOfCards = [],
 
+};
+
+var Player1 ("Test Player");
+console.log(Player1);
+
+}
 
 
 });
