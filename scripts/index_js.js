c6cfb1ccdbcb751fc07165eab7c95999a35951de
Jason Slavin <slavin.jhs@gmail.com>
2017-01-09 22:42:45 -0800

    more work

--- scripts/index_js.js
+++ scripts/index_js.js
@@ -7,9 +7,41 @@ function Player (name) {
 	this.handOfCards = [];
 };
 
-var Player1 = new  Player("Test Player");
-console.log(Player1);
+function Dealer (name, deckOfCards) {
+	this.name = name;
+	this.deckOfCards = deckOfCards;
+	this.dealerHand = [];
 
+	function dealHandsToPlayersAndSelf () {
+		//Will Need to set this variable inside of a for loop, making the variable each time, or rather
+		//make another function that chooses the random card.......
+		var cardToDeal = Math.floor((Math.random() * this.deckOfCards.length) + 1);
+	}
+
+};
+
+const noSuitDeck = [];
+
+function createDeck (array) {
+	for (var i = 2; i < 12; i++) {
+		for (var numTimes = 0; numTimes < 4; numTimes++){
+			array.push(i);
+		}
+	}
+	for (var i = 0; i < 12; i++) {
+		array.push(10);
+	}
+}
+
+
+createDeck(noSuitDeck);
+
+var Player1 = new  Player("Test Player");
+var beginningDealer = new Dealer("Smokey Joe", noSuitDeck);
+console.log(Player1);
+console.log(beginningDealer);
+console.log(beginningDealer.deckOfCards);
+console.log("You made this deck:\n" + noSuitDeck);
 
 
 
