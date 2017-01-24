62971289dbef4ad866e82a15c80bfebc68c628ec
Jason Slavin <slavin.jhs@gmail.com>
2017-01-24 09:40:17 -0800

    Working on making the game deal each time, and making buttons like hit and stay unaccesible until deal has been hit

--- scripts/blackjack.js
+++ scripts/blackjack.js
@@ -8,6 +8,8 @@ function Player (name, position) {
 	this.handOfCards = [];
 	this.bustStatus = false;
 	this.stayStatus = false;
+	this.winStatus = false;
+	this.sumOfHand = 0;
 
 	this.turnCardsIntoDealer = function () {
 		//this function needs to clear hand OfCards - called on deal click
@@ -17,7 +19,7 @@ function Player (name, position) {
 
 	this.stayKeepCards = function () {
 		console.log("I am deciding to stay");
-		if (this.stayStatus === false) {
+		if (this.stayStatus === false && haveCardsBeenDealtByDealer === true) {
 			console.log("returning a true stay status");
 			return this.stayStatus = true;
 		}
@@ -30,6 +32,9 @@ function Dealer (name, deckOfCards) {
 	this.deckOfCards = deckOfCards;
 	this.handOfCards = []; //could i use a prototype?
 	this.stayStatus = false;
+	this.bustStatus = false;
+	this.winStatus = false;
+	this.sumOfHand = 0;
 
 	this.shuffleDeck = function (deck){
 	    let counter = deck.length;
@@ -82,18 +87,17 @@ function Dealer (name, deckOfCards) {
 			} else {
 				this.stayStatus = true;
 				return this.stayStatus;
-			};
-		};
-	}
+			}
+		}
+	};
 
-	this.checkIfPlayerBust = function (player) {
-		let playerCardSum = player.handOfCards.reduce(function (a, b) {
-			return a + b;
-		}, 0);
+	this.checkIfCardTakerBust = function (object) {
+		let playerCardSum = object.handOfCards.reduce(function (a, b) {return a + b;}, 0);
+
 		if (playerCardSum > 21) {
-			player.bustStatus = true;
-			Table.playerHasBusted(player);
-			return player.bustStatus;
+			object.bustStatus = true;
+			Table.cardTakerHasBusted(object);
+			return object.bustStatus;
 		}
 	};
 
@@ -103,9 +107,38 @@ function Dealer (name, deckOfCards) {
 				console.log("Player has stayed - Dealer Function holder");
 				this.hitSelf(player);
 				table.dealerHitOnTable(this, player);
+				this.checkIfCardTakerBust(this);
+				this.checkWhoWon(player);
+				table.someoneHasWon(player);
+				table.someoneHasWon(this);
+				window.setTimeout(table.newHandEraseNumbers, 5000);
+				window.setTimeout(table.newHandEraseCards, 5000);
 			}
 		}
+	};
+
+	this.checkWhoWon = function (player) {
+		this.sumOfHand = this.handOfCards.reduce(function (a, b){ return a + b;}, 0);
+		player.sumOfHand = player.handOfCards.reduce(function (a, b){ return a + b;}, 0);
+		if(player.bustStatus === false && this.bustStatus === true){
+			player.winStatus = true;
+		} else if (player.bustStatus === true && this.bustStatus === false) {
+			this.winStatus = true;
+		} else if (this.sumOfHand > player.sumOfHand && this.bustStatus !== true) {
+			this.winStatus = true;
+		} else if (this.sumOfHand < player.sumOfHand && player.bustStatus !== true) {
+			player.winStatus = true;
+		}
 	}
+	
+	this.askForNewGame = function (player, table) {
+		if (player.winStatus === true || this.winStatus === true) {
+			window.setTimeout(function(){return true}, 5000);
+
+			return true;
+		}
+		return true;
+	};
 
 	this.newDealerHand = function () {
 		//this needs to emoty handOfCards .... called on "Deal" click
@@ -175,28 +208,44 @@ var Table = {
 	},
 
 	newHandEraseNumbers : function () {
-		return true;
+		for (var i = 0; i <= 9; i++){
+			var cardsToPickup = document.getElementsByClassName("card");
+			cardsToPickup.innerHTML = "";
+		}
 		//remove old innerHTML
 	},
 
 	newHandEraseCards : function () {
-		return true;
-		//change attributes to "remove cards"
+		for (var i = 0; i <= 9; i++){
+			var cardsToPickup = document.getElementsByClassName("card");
+			cardsToPickup[i].setAttribute("class", "card hiddenCard");
+		}
 	}, 
 
-	playerHasBusted : function (player) {
-		if (player.bustStatus === true) {
+	cardTakerHasBusted : function (object) {
+		if (object.bustStatus === true) {
 			let bustBanner = document.getElementsByClassName("spacer")[0];
-			bustBanner.innerHTML = "You have Busted";
+			bustBanner.innerHTML = object.name + " has Busted";
 			window.setTimeout(function(){bustBanner.innerHTML = "";}, 3000);
 		}
 	},
+
+	someoneHasWon : function (object) {
+		if (object.winStatus === true) {
+			let winBanner = document.getElementsByClassName("spacer")[0];
+			winBanner.innerHTML = object.name + " has Won!";
+			window.setTimeout(function(){winBanner.innerHTML = "";}, 4000);
+		}
+		let dealButtonName = document.getElementById("dealCards");
+		dealButtonName.innerHTML = "Deal Again?";
+	}
 };
 
 
 //Array for the first Deck
+//Should the deck belong to the Table? 
 const noSuitDeck = [];
-//Creates the first Decl
+//Creates the first Deck
 function createDeck (array) {
 	for (var i = 2; i < 12; i++) {
 		for (var numTimes = 0; numTimes < 4; numTimes++){
@@ -224,6 +273,11 @@ let haveCardsBeenDealtByDealer = false;
 let playerHandSum = 0;
 
 //logs out properties
+function turnBlue (card) {
+	var bluecard = document.getElementsByClassName("card");
+	bluecard[card].setAttribute("class", "blueCard");
+}
+
 
 //event listeners go here:
 dealButton.addEventListener("click", function () {beginningDealer.shuffleDeck(noSuitDeck);});
@@ -231,7 +285,7 @@ dealButton.addEventListener("click", function () {beginningDealer.dealCardstoPla
 dealButton.addEventListener("click", function () {Table.dealCardsToTable(Player1, beginningDealer);});
 hitButton.addEventListener("click", function () {beginningDealer.hitPlayer(Player1);});
 hitButton.addEventListener("click", function () {Table.playerHitOnTable(beginningDealer, Player1);});
-hitButton.addEventListener("click", function () {beginningDealer.checkIfPlayerBust(Player1);});
+hitButton.addEventListener("click", function () {beginningDealer.checkIfCardTakerBust(Player1);});
 stayButton.addEventListener("click", function () {Player1.stayKeepCards();});
 stayButton.addEventListener("click", function () {beginningDealer.playerHasStayed(Player1, Table);});
 
